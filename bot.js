205e44fec07fa2f5d585eb127959221f642d1736
Kierancz <kieran.czerwinski@colorado.edu>
2017-04-04 03:01:16 -0600

    added tweetIt and followed functions

--- bot.js
+++ bot.js
@@ -3,18 +3,52 @@ console.log("The bot is starting")
 var Twit = require('twit')
 var config = require('./config')
 
-console.log(config)
 var T = new Twit(config)
-
 
 var params = { 
   q: 'banana since:2011-07-11', 
-  count: 100 
+  count: 2 
 }
 
+//Sets up user stream
+var stream = T.stream('user')
+
+//Anytime someone follows us
+stream.on('follow', followed)
+
+function followed(eventMsg) {
+  console.log("follow event ")
+  var name = eventMsg.source.name
+  var screenName = eventMsg.source.screen_name
+  tweetIt('@' + screenName + ' Thank you for following transitBot!')
+}
 
 T.get('search/tweets', params, gotData)
 
 function gotData(err, data, response) {
-  console.log(data)
+  var tweets = data.statuses;
+  for(var i = 0; i < tweets.length; i++){
+    console.log(tweets[i].text)
+  }
+  //console.log(data)
+}
+
+tweetIt();
+
+function tweetIt(txt) {
+  var tweet = {
+    status: txt
+  }
+
+  T.post('statuses/update', tweet, tweeted)
+
+  function tweeted(err, data, response) {
+    if(err) {
+      console.log("something went wrong")
+    }
+    else {
+      console.log("it tweeted!")
+    }
+    console.log(data)
+  }
 }
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   consumer_key:         '5VlptZ22djGOM5DhmxRmk5C5D',
-  consumer_secret:      '6CMJUZbxIc2UiybCiBPDC8iHlk1jG2V4NKgupi6ejpRycpCCfg ',
+  consumer_secret:      '6CMJUZbxIc2UiybCiBPDC8iHlk1jG2V4NKgupi6ejpRycpCCfg',
   access_token:         '2700145044-NtupcUloM2msqvHt4AzSTRmXZe6yQSwgM5TmxX8',
-  access_token_secret:  ' EVZioMFqV6SCjeUQmZURbz6kFHxagwRLK23VSOzd4vvJt',
+  access_token_secret:  'EVZioMFqV6SCjeUQmZURbz6kFHxagwRLK23VSOzd4vvJt',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
 }
