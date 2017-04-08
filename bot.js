7bdfbef6a7e352d9e13e94d83c64b4dc002b6df0
Kierancz <kieran.czerwinski@colorado.edu>
2017-04-07 23:49:26 -0600

    added retweet function

--- bot.js
+++ bot.js
@@ -1,31 +1,24 @@
 console.log("The bot is starting")
-
 var Twit = require('twit')
 var config = require('./config')
 
 var T = new Twit(config)
 
+//tweetIt();
+
+
+// gotData()
+//
 var params = { 
-  q: 'banana since:2011-07-11', 
-  count: 2 
+  q: 'global warming OR climate change :) since:2015-12-21', 
+  count: 20 
 }
-
-//Sets up user stream
-var stream = T.stream('user')
-
-//Anytime someone follows us
-stream.on('follow', followed)
-
-function followed(eventMsg) {
-  console.log("follow event ")
-  var name = eventMsg.source.name
-  var screenName = eventMsg.source.screen_name
-  tweetIt('@' + screenName + ' Thank you for following transitBot!')
-}
-
 T.get('search/tweets', params, gotData)
 
 function gotData(err, data, response) {
+  if(err) console.log("there was an error: ", err)
+  else console.log("data received successfully")
+
   var tweets = data.statuses;
   for(var i = 0; i < tweets.length; i++){
     console.log(tweets[i].text)
@@ -33,8 +26,8 @@ function gotData(err, data, response) {
   //console.log(data)
 }
 
-tweetIt();
-
+// tweetIt()
+//
 function tweetIt(txt) {
   var tweet = {
     status: txt
@@ -43,12 +36,33 @@ function tweetIt(txt) {
   T.post('statuses/update', tweet, tweeted)
 
   function tweeted(err, data, response) {
-    if(err) {
-      console.log("something went wrong")
-    }
-    else {
-      console.log("it tweeted!")
-    }
+    if(err) console.log("something went wrong: ", err)
+    else console.log("it tweeted!", response)
+
     console.log(data)
+  }
+}
+
+// followed()
+//
+//Sets up user stream
+var stream = T.stream('user')
+//Anytime someone follows us
+stream.on('follow', followed)
+function followed(eventMsg) {
+  console.log("follow event ")
+  var name = eventMsg.source.name
+  var screenName = eventMsg.source.screen_name
+  tweetIt('@' + screenName + ' Thank you for following transitBot!')
+}
+
+// 
+//  reTweet()
+//
+function reTweet(tweetId) {
+  T.post('statuses/retweet/:id', tweetId, reTweetIt)
+
+  function reTweetIt(err, data, response) {
+    console.log("reTweet data: ", data)
   }
 }
