0548a9c24c4bc2f5c2e5700fc55c992ffb71fff7
Kieran <kieran.czerwinski@colorado.edu>
2017-05-03 03:12:41 -0600

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         'j4Q12XEyKVuYERZOiyBpQOZO9',
-  consumer_secret:      '3HNe9vPfZXTOlcFiZxqx8E8LySC9H3iakeedvLK1wrThrbLFwy',
-  access_token:         '858131679730380802-D9iIPsOefayMeBGYeM4VmPkElASKKEN',
-  access_token_secret:  '8Y6rhYmlSHcpEQtpZLwDWIwIq2VfpyY9R8pjIXiic9uyE',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
-}
